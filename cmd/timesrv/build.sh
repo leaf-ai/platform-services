93468eebdb71ff13a98a6c33eb80080809b7cdde
Karl Mutch <karlmutch@gmail.com>
2017-12-18 13:13:14 -0800

    Package reorg moved the version variables

--- cmd/timesrv/build.sh
+++ cmd/timesrv/build.sh
@@ -13,11 +13,11 @@ go get -u -f github.com/golang/dep/cmd/dep
 go get -u -f github.com/aktau/github-release
 dep ensure -no-vendor
 mkdir -p cmd/timesrv/bin
-go build -ldflags "-X version.BuildTime=$DATE -X version.GitHash=$HASH" -o cmd/timesrv/bin/timesrv cmd/timesrv/*.go
-go build -ldflags "-X version.BuildTime=$DATE -X version.GitHash=$HASH" -race -o cmd/timesrv/bin/timesrv-race cmd/timesrv/*.go
-go test -ldflags "-X command-line-arguments.TestRunMain=Use -X command-line-arguments.BuildTime=$DATE -X command-line-arguments.GitHash=$HASH" -coverpkg="." -c -o cmd/timesrv/bin/timesrv-run-coverage cmd/timesrv/*.go
-go test -ldflags "-X command-line-arguments.BuildTime=$DATE -X command-line-arguments.GitHash=$HASH" -coverpkg="." -c -o bin/timesrv-test-coverage cmd/timesrv/*.go
-go test -ldflags "-X command-line-arguments.BuildTime=$DATE -X command-line-arguments.GitHash=$HASH" -race -c -o cmd/timesrv/bin/timesrv-test cmd/timesrv/*.go
+go build -ldflags "-X github.com/KarlMutch/MeshTest/version.BuildTime=$DATE -X github.com/KarlMutch/MeshTest/version.GitHash=$HASH" -o cmd/timesrv/bin/timesrv cmd/timesrv/*.go
+go build -ldflags "-X github.com/KarlMutch/MeshTest/version.BuildTime=$DATE -X github.com/KarlMutch/MeshTest/version.GitHash=$HASH" -race -o cmd/timesrv/bin/timesrv-race cmd/timesrv/*.go
+go test -ldflags "-X github.com/KarlMutch/MeshTest/version.TestRunMain=Use -X github.com/KarlMutch/MeshTest/version.BuildTime=$DATE -X github.com/KarlMutch/MeshTest/version.GitHash=$HASH" -coverpkg="." -c -o cmd/timesrv/bin/timesrv-run-coverage cmd/timesrv/*.go
+go test -ldflags "-X github.com/KarlMutch/MeshTest/version.BuildTime=$DATE -X github.com/KarlMutch/MeshTest/version.GitHash=$HASH" -coverpkg="." -c -o bin/timesrv-test-coverage cmd/timesrv/*.go
+go test -ldflags "-X github.com/KarlMutch/MeshTest/version.BuildTime=$DATE -X github.com/KarlMutch/MeshTest/version.GitHash=$HASH" -race -c -o cmd/timesrv/bin/timesrv-test cmd/timesrv/*.go
 if ! [ -z ${TRAVIS_TAG+x} ]; then
     if ! [ -z ${GITHUB_TOKEN+x} ]; then
         github-release release --user karlmutch --repo MeshTest --tag ${TRAVIS_TAG} --pre-release && \
