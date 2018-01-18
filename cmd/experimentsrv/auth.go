34e761994b895ac48cd832ac3048854a671256b0
Karl Mutch <karlmutch@gmail.com>
2018-01-17 16:21:41 -0800

    Authenticator refactored for inclusion into gRPC

--- cmd/experimentsrv/auth.go
+++ cmd/experimentsrv/auth.go
@@ -1,14 +1,71 @@
 package main
 
 // This file contains the implementation of per request authentication
-// for our gRPC server
+// for our gRPC server.  Authetication and validation are done using the Auth0
+// platform.
 
 import (
+	"flag"
+	"fmt"
+	"strings"
+
+	"net/http"
+
+	"github.com/go-stack/stack"
+	"github.com/karlmutch/errors"
+
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/metadata"
+
+	"github.com/auth0-community/go-auth0"
+	"gopkg.in/square/go-jose.v2"
 )
+
+var (
+	auth0Domain = flag.String("auth0-domain", "sentientai.auth0.com", "The domain assigned to the server API by Auth0")
+)
+
+func validateToken(token string, claimCheck string) (err errors.Error) {
+
+	client := auth0.NewJWKClient(auth0.JWKClientOptions{
+		URI: "https://" + *auth0Domain + "/.well-known/jwks.json",
+	})
+	audience := []string{
+		"http://api.sentient.ai/experimentsrv",
+	}
+
+	configuration := auth0.NewConfiguration(client, audience, "https://"+*auth0Domain+"/", jose.RS256)
+	validator := auth0.NewValidator(configuration)
+
+	headerTokenRequest, errGo := http.NewRequest("", audience[0], nil)
+	if errGo != nil {
+		return errors.Wrap(errGo).With("stack", stack.Trace().TrimRuntime())
+	}
+	headerValue := fmt.Sprintf("Bearer %s", *auth0TestToken)
+	headerTokenRequest.Header.Add("Authorization", headerValue)
+
+	validResp, errGo := validator.ValidateRequest(headerTokenRequest)
+	if errGo != nil {
+		return errors.Wrap(errGo).With("stack", stack.Trace().TrimRuntime())
+	}
+
+	if len(claimCheck) == 0 {
+		return nil
+	}
+
+	claims := map[string]interface{}{}
+	errGo = validator.Claims(headerTokenRequest, validResp, &claims)
+	if errGo != nil {
+		return errors.Wrap(errGo).With("stack", stack.Trace().TrimRuntime())
+	}
+
+	if !strings.Contains(claims["scope"].(string), claimCheck) {
+		return errors.New(fmt.Sprintf("the authenticated user does not have the appropriate '%s' scope", claimCheck)).With("stack", stack.Trace().TrimRuntime())
+	}
+	return nil
+}
 
 func authInterceptor(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
 	meta, ok := metadata.FromIncomingContext(ctx)
@@ -22,8 +79,8 @@ func authInterceptor(ctx context.Context, req interface{}, info *grpc.UnaryServe
 	if len(meta["authorization"]) != 1 {
 		return nil, grpc.Errorf(codes.Unauthenticated, "invalid token")
 	}
-	if meta["authorization"][0] != "valid-token" {
-		return nil, grpc.Errorf(codes.Unauthenticated, "invalid token")
+	if err := validateToken(meta["authorization"][0], "all:experiments"); err != nil {
+		return nil, grpc.Errorf(codes.Unauthenticated, err.Error())
 	}
 
 	return handler(ctx, req)
--- cmd/experimentsrv/auth0_test.go
+++ cmd/experimentsrv/auth0_test.go
@@ -2,43 +2,16 @@ package main
 
 import (
 	"flag"
-	"fmt"
-	"net/http"
 	"testing"
-
-	"github.com/auth0-community/go-auth0"
-	"gopkg.in/square/go-jose.v2"
 )
 
 var (
-	auth0Domain    = flag.String("auth0-domain", "sentientai.auth0.com", "The domain assigned to the server API by Auth0")
 	auth0TestToken = flag.String("auth0-token", "", "The raw token string received from an invocation of {auth0-domain}/oauth/token}")
 )
 
 func TestAuth0(t *testing.T) {
-	client := auth0.NewJWKClient(auth0.JWKClientOptions{
-		URI: "https://" + *auth0Domain + "/.well-known/jwks.json",
-	})
-	audience := []string{
-		"http://api.sentient.ai/experimentsrv",
+	err := validateToken(*auth0TestToken, "all:experiments")
+	if err != nil {
+		t.Error("Expected nil, got ", err.Error())
 	}
-
-	configuration := auth0.NewConfiguration(client, audience, "https://"+*auth0Domain+"/", jose.RS256)
-	validator := auth0.NewValidator(configuration)
-
-	headerTokenRequest, _ := http.NewRequest("", audience[0], nil)
-	headerValue := fmt.Sprintf("Bearer %s", *auth0TestToken)
-	headerTokenRequest.Header.Add("Authorization", headerValue)
-
-	token, errGo := validator.ValidateRequest(headerTokenRequest)
-	if errGo != nil {
-		t.Error("Expected nil, got ", errGo.Error())
-	}
-	logger.Info(fmt.Sprintf("%+v", token))
-	claims := map[string]interface{}{}
-	errGo = validator.Claims(headerTokenRequest, token, &claims)
-	if errGo != nil {
-		t.Error("Expected nil, got ", errGo.Error())
-	}
-	logger.Info(fmt.Sprintf("%+v", claims))
 }
