6f3e92048943938365261286a491929f064858ec
Karl Mutch <karlmutch@gmail.com>
2019-05-23 13:37:22 -0700

    Improve the build scripts

--- all-build.sh
+++ all-build.sh
@@ -4,53 +4,21 @@ go install github.com/golang/dep/cmd/dep
 go get github.com/karlmutch/duat
 go install github.com/karlmutch/duat/cmd/semver
 mkdir -p internal/gen
-./cmd/experimentsrv/build.sh gen
-if [ $? -ne 0 ]; then
-    echo "experimentsrv build failed"
-    exit $?
-fi
-./cmd/downstream/build.sh gen
-if [ $? -ne 0 ]; then
-    echo "experimentsrv build failed"
-    exit $?
-fi
-./cmd/echosrv/build.sh gen
-if [ $? -ne 0 ]; then
-    echo "echosrv build failed"
-    exit $?
-fi
-./cmd/timesrv/build.sh gen
-if [ $? -ne 0 ]; then
-    echo "timesrv build failed"
-    exit $?
-fi
-./cmd/restpoc/build.sh gen
-if [ $? -ne 0 ]; then
-    echo "restpoc build failed"
-    exit $?
-fi
-./cmd/experimentsrv/build.sh
-if [ $? -ne 0 ]; then
-    echo "experimentsrv build failed"
-    exit $?
-fi
-./cmd/downstream/build.sh
-if [ $? -ne 0 ]; then
-    echo "experimentsrv build failed"
-    exit $?
-fi
-./cmd/echosrv/build.sh 
-if [ $? -ne 0 ]; then
-    echo "echosrv build failed"
-    exit $?
-fi
-./cmd/timesrv/build.sh
-if [ $? -ne 0 ]; then
-    echo "timesrv build failed"
-    exit $?
-fi
-./cmd/restpoc/build.sh
-if [ $? -ne 0 ]; then
-    echo "restpoc build failed"
-    exit $?
-fi
+
+for dir in cmd/*/ ; do
+    base="${dir%%\/}"
+    base="${base##*/}"
+    if [ "$base" == "cli-downstream" ] ; then
+        continue
+    fi
+    $dir/build.sh gen
+    if [ $? -ne 0 ]; then
+        echo "$base code generator failed"
+        exit $?
+    fi
+    $dir/build.sh
+    if [ $? -ne 0 ]; then
+        echo "$base code compilation failed"
+        exit $?
+    fi
+done
